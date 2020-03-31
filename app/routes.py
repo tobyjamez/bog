0696b8f89e4595557b39faa6df971f61a9b68b41
tobyjamez <tobyswjames@gmail.com>
2020-03-31 16:13:06 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+from app.generate import generate_tiles
 
 @app.route("/")
 def index():
@@ -7,4 +8,18 @@ def index():
 
 @app.route("/generate")
 def generate():
-    return render_template("
+    tiles = generate_tiles()
+    return render_template("game.html", tiles=tiles)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/create")
+def create():
+    name = request.form['name']
+    return render_template("name.html")
+
+@app.route("/join")
+def join():
+    return render_template("join.html")
